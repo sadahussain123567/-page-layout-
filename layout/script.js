63f70752527d4710dc2f1a447c2c4c11def8acc6
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2024-10-21 19:04:42 +0500

    Add files via upload

---
+++ layout/script.js
@@ -0,0 +1,5 @@
+// const menu = document.querySelector(".menubars")
+// const navbar = document.querySelector(".navlist")
+// menu.addEventListener("click",()=>{
+//     navbar.classList.toggle("active")
+// })
