f88e509d092de6c118039d2c096a4d8baed2ee85
saddam hussain <159890907+sadahussain123567@users.noreply.github.com>
2024-10-21 19:17:13 +0500

    Update script.js

--- layout/script.js
+++ layout/script.js
@@ -1,5 +1,5 @@
-// const menu = document.querySelector(".menubars")
-// const navbar = document.querySelector(".navlist")
-// menu.addEventListener("click",()=>{
-//     navbar.classList.toggle("active")
-// })
+const menu = document.querySelector(".menubars")
+const navbar = document.querySelector(".navlist")
+  menu.addEventListener("click",()=>{
+  navbar.classList.toggle("active")
+  })
